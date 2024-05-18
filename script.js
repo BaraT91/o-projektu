e8809000048a4c66ac4f19a6b8bb744c4a27a4ee
Tatiana Kohoutova <tatiana.belozerova1543@gmail.com>
2024-05-18 09:38:08 +0200

    responzivni menicko

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+console.log('funguji');
 
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const menuClassic = document.querySelector('.menu');
+
+const hamburgerLines = document.querySelectorAll('.line');
+
+console.log(hamburgerLines);
+
+hamburgerMenu.addEventListener('click', () => {
+  menuClassic.classList.toggle('open');
+});
+
+hamburgerLines.forEach((prvek) => {
+  hamburgerMenu.addEventListener('click', () => {
+    prvek.classList.toggle('open');
+  });
+});
