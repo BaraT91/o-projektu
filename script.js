0badf7090d6d1321d7f569721719a8a4f20c4dfa
Barbora Tesařová <tesarovabara@seznam.cz>
2024-05-22 13:02:05 +0200

    Úprava tlačítek na úvodu. Oprava veverek na O hře a O nás.

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-console.log('funguji');
-
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const menuClassic = document.querySelector('.menu');
 
 const hamburgerLines = document.querySelectorAll('.line');
-
-console.log(hamburgerLines);
 
 hamburgerMenu.addEventListener('click', () => {
   menuClassic.classList.toggle('open');
